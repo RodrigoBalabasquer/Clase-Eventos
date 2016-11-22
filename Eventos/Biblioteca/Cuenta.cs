9722edd6aebb7a8fab37cea1b13d2f88202e427e
unknown <alumno@LAB02PC03.labs.extension>
2016-11-22 10:31:16 -0300

    Clase donde implementamos eventos

---
+++ Eventos/Biblioteca/Cuenta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{   
+    public delegate void CuentaDelegado(float dinero);
+    public class Cuenta
+    {
+        public float saldo;
+        public void SacarDinero(float dinero) 
+        {
+            if (dinero > this.saldo) 
+            {
+                //si estubieramos en aplicacion de consola es Console.write
+                //si estubieramos en windows forms es Messagefox.show
+                //si estubieramos en una pagina web seria un alert
+                SaldoNegativo(dinero);
+            }
+        }
+        public event CuentaDelegado SaldoNegativo;
+    }
+}
---
+++ Eventos/Ejemplo1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Ejemplo1
+{
+    public partial class Form1 : Form
+    {
+        public Biblioteca.Cuenta cuenta;
+        public Form1()
+        {
+            InitializeComponent();
+            this.cuenta = new Biblioteca.Cuenta();
+            this.cuenta.SaldoNegativo += this.MostrarSaldoNegativo;
+        }
+
+        public void MostrarSaldoNegativo(float dinero) 
+        {
+            MessageBox.Show(dinero.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.cuenta.SacarDinero(1000);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show("textBox1_KeyPress");
+        }
+        private void textBox1_KeyPress2(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show("textBox1_KeyPress2");
+        }
+        private void mio(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show("mio");
+        }
+    }
+}
